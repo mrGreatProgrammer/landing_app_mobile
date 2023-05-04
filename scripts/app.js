2cc106dd3b7327f6e39e3f7e558f350848421f24
FarhodD <farhoddavlatov0120@gmail.com>
2023-05-04 22:16:35 +0500

    adaptive

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
 const currentYearEl = document.querySelector('.current__year');
 const currentYear = new Date().getFullYear();
+const burgerMenu = document.querySelector('.burger__menu');
+const submenu = document.querySelector('.submenu');
+
+
+function toggleMenu() {
+  burgerMenu.classList.toggle("active");
+  submenu.classList.toggle("vis__submenu");
+}
 
 currentYearEl.textContent= currentYear;
